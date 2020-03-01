8a1ecfae1cf3d61789ede72dc4c742c2bc3a51d0
Rodolpho Rosa <rodolphorosa05@gmail.com>
2020-03-01 19:34:04 -0300

    configuração de porta do servidor

--- app.py
+++ app.py
@@ -199,4 +199,7 @@ if __name__ == "__main__":
 
     app.secret_key = 'super secret key'
     app.config["DEBUG"] = True
-    app.run()
+    # app.run()
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
